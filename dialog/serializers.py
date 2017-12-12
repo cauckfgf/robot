bd86160e34f525cd68af3e03cacedef9d9971d8b
fgf <fgf@bimsheng.com>
2017-12-12 12:40:10 +0800

    c

--- dialog/serializers.py
+++ dialog/serializers.py
@@ -1,45 +1,32 @@
 # -*- coding: utf-8 -*-
 from rest_framework import serializers
-
 from .models import *
 
-class DirectorySerializer(serializers.HyperlinkedModelSerializer):
-    '''文件夹'''
+class ActionSerializer(serializers.HyperlinkedModelSerializer):
+    '''机电系统类型'''
     id = serializers.ReadOnlyField()
     class Meta:
-        model = Directory
-        fields = ('id','name','parent','createtime','url')
-
-class DocumentSerializer1(serializers.HyperlinkedModelSerializer):
-    '''文件'''
-    id = serializers.ReadOnlyField()
-    fileurl = serializers.ReadOnlyField()
-    class Meta:
-        model = Document
+        model = Action
         fields = '__all__'
 
-class Doc2RelateSerializer(serializers.HyperlinkedModelSerializer):
-    '''文件关联文件夹'''
+class KeywordSerializer(serializers.HyperlinkedModelSerializer):
+    '''机电系统类型'''
     id = serializers.ReadOnlyField()
-    document = DocumentSerializer1(read_only=True)
-    relate = serializers.ReadOnlyField()
     class Meta:
-        model = Doc2Relate
+        model = Keyword
         fields = '__all__'
 
-class Doc2RelateSerializer1(serializers.HyperlinkedModelSerializer):
-    '''文件关联文件夹'''
+class AnswerSerializer(serializers.HyperlinkedModelSerializer):
+    '''机电系统类型'''
     id = serializers.ReadOnlyField()
-    relate = serializers.ReadOnlyField()
     class Meta:
-        model = Doc2Relate
+        model = Answer
         fields = '__all__'
 
-class DocumentSerializer(serializers.HyperlinkedModelSerializer):
-    '''文件'''
+class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    '''机电系统类型'''
     id = serializers.ReadOnlyField()
-    fileurl = serializers.ReadOnlyField()
-    Doc2Relates = Doc2RelateSerializer1(many=True,read_only=True)
     class Meta:
-        model = Document
+        model = Question
         fields = '__all__'
+
