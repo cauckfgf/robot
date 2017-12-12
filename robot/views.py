3117f23d4902547212088479b7844fc4f47d3104
fgf <fgf@bimsheng.com>
2017-12-12 14:42:19 +0800

    c

--- robot/views.py
+++ robot/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render,render_to_response
 from django.template import loader,Context,RequestContext
-def index_view(request):
+def index(request):
     return render_to_response('index.html', RequestContext(request,locals()))
