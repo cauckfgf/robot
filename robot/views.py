728ef35ca4472399edc03c9d7bc4abe40f241f69
fgf <fgf@bimsheng.com>
2017-12-12 14:47:00 +0800

    c

--- robot/views.py
+++ robot/views.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render,render_to_response
 from django.template import loader,Context,RequestContext
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+
+@csrf_exempt
 def index(request):
     return render_to_response('index.html', RequestContext(request,locals()))
